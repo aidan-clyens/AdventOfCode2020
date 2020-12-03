a0e1da4e177e536d42b0eceb40ea7091844e14be
Aidan Clyens <aidanc@rogers.com>
2020-12-03 09:43:37 -0500

    Optimize part 1

--- Day1/expense_report.py
+++ Day1/expense_report.py
@@ -2,10 +2,18 @@ data = []
 with open("input.txt", "r") as f:
     data = [int(l.replace("\n", "")) for l in f.readlines()]
 
+data = sorted(data)
+S = 2020
 
-for a in data:
-    for b in data:
-        for c in data:
-            if a + b + c== 2020:
-                print(f"{a} * {b} * {c} = {a*b*c}")
-                exit()
+i = 0
+j = len(data) - 1
+
+while i < j:
+    if data[i] + data[j] == S:
+        print(f"{data[i]} * {data[j]} = {data[i]*data[j]}")
+        break
+    elif data[i] + data[j] < S:
+        i += 1
+    else:
+        j -= 1
+
