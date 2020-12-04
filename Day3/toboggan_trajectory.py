b2f06c1bd8fe1fb7bbc79134f72e61f2342aa264
Aidan Clyens <aidanc@rogers.com>
2020-12-03 20:28:58 -0500

    Finish part 1

--- Day3/toboggan_trajectory.py
+++ Day3/toboggan_trajectory.py
@@ -4,6 +4,20 @@ if __name__ == '__main__':
         for l in f.readlines():
             l = l.replace("\n", "")
             
-            trees.append([(c == ".") for c in l])
+            trees.append([(c == "#") for c in l])
 
-    print(trees)
+    height = len(trees)
+    width = len(trees[0])
+
+    x = 0
+    y = 0
+    trees_count = 0
+    while y < height - 1:
+        x = (x + 3) % width
+        y += 1
+
+        if trees[y][x]:
+            trees_count += 1
+
+    print("Part 1")
+    print(trees_count)
