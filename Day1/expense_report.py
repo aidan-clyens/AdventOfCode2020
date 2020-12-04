de54098b3036357952425409834e42c59ebcce1d
Aidan Clyens <aidanc@rogers.com>
2020-12-03 19:15:33 -0500

    Optimize part 2

--- Day1/expense_report.py
+++ Day1/expense_report.py
@@ -1,19 +1,36 @@
-data = []
-with open("input.txt", "r") as f:
-    data = [int(l.replace("\n", "")) for l in f.readlines()]
+def sum2(data, S):
+    i = 0
+    j = len(data) - 1
 
-data = sorted(data)
-S = 2020
+    while i < j:
+        if data[i] + data[j] == S:
+            return [data[i], data[j]]
+        elif data[i] + data[j] < S:
+            i += 1
+        else:
+            j -= 1
 
-i = 0
-j = len(data) - 1
 
-while i < j:
-    if data[i] + data[j] == S:
-        print(f"{data[i]} * {data[j]} = {data[i]*data[j]}")
-        break
-    elif data[i] + data[j] < S:
-        i += 1
-    else:
-        j -= 1
+def sum3(data, S):
+    for i in range(len(data)):
+        d = sum2(data[i+1:], S - data[i])
+        if d is not None:
+            if data[i] + d[0] + d[1] == S:
+                return d + [data[i]]
 
+
+if __name__ == "__main__":
+    data = []
+    with open("input.txt", "r") as f:
+        data = [int(l.replace("\n", "")) for l in f.readlines()]
+
+    data = sorted(data)
+    S = 2020
+
+    print("Part 1:")
+    [a, b] = sum2(data, S)
+    print(f"{a} * {b} = {a*b}")
+    
+    print("Part 2:")
+    [a, b, c] = sum3(data, S)
+    print(f"{a} * {b} * {c} = {a*b*c}")
