f96de2b370dacef9015bf22210639686c08e19e5
Aidan Clyens <aidanc@rogers.com>
2020-12-06 14:30:55 -0500

    Finish part 1

---
+++ Day4/passports.py
@@ -0,0 +1,34 @@
+import re
+
+
+def is_valid(passport):
+    expected_keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+    for k in expected_keys:
+        if k not in passport.keys():
+            return False
+
+    return True
+
+
+if __name__ == '__main__':
+    passports = []
+    passports.append({})
+    i = 0
+    with open("input.txt", "r") as f:
+        for l in f.readlines():
+            l = l.replace("\n", "").split()
+            if len(l) == 0:
+                i+= 1
+                passports.append({})
+            else:
+                for entry in l:
+                    entry = entry.split(":")
+                    passports[i][entry[0]] = entry[1]
+
+    valid_count = 0
+    for p in passports:
+        if is_valid(p):
+            valid_count += 1
+
+    print("Part 1:")
+    print(valid_count)
