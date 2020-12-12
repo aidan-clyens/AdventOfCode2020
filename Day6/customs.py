ce81ffe2181856b4dedd0d5b76afe4aac332f3c3
Aidan Clyens <aidanc@rogers.com>
2020-12-12 13:51:24 -0500

    Add Day 6 part 1

---
+++ Day6/customs.py
@@ -0,0 +1,25 @@
+if __name__ == '__main__':
+    groups = []
+    with open("input.txt", "r") as f:
+        group = []
+        for line in f.readlines():
+            line = line.replace("\n", "")
+            if len(line) == 0:
+                groups.append(group)
+                group = []
+            else:
+                group.append(line)
+        groups.append(group)
+
+    total_sum = 0
+    for group in groups: 
+        answers = []
+        for person in group:
+            for c in person:
+                if c not in answers:
+                    answers.append(c)
+
+        total_sum += len(answers)
+
+    print("Part 1:")
+    print(total_sum)
